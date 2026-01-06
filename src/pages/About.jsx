92e4d368706d62c8f006e1fdc40228907bd03c29
boburdv <boburdv@gmail.com>
2026-01-06 13:27:14 -0700

    Update src/pages/About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,10 @@
 export default function About() {
   return (
-    <div className="max-w-6xl mx-auto mt-36 mb-24 px-4">
+    <div className="max-w-6xl mx-auto mt-29 lg:mt-36 mb-24 px-4">
       <div className="bg-base-100 overflow-hidden">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 p-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:gap-8 gap-4">
           <div className="w-full h-72 md:h-auto overflow-hidden">
-            <img src="/home-img.jpg" alt="About Image" className="w-full h-full object-cover" />
+            <img src="/home-img.jpg" alt="About Image" className="w-full h-full object-cover rounded-md" />
           </div>
 
           <div className="flex flex-col justify-center space-y-6">
@@ -25,8 +25,9 @@ export default function About() {
 
             <div>
               <h2 className="text-xl font-semibold mb-3">Aloqa</h2>
-              <p className="text-gray-700 font-medium">+998 90 000 00 00</p>
-              <p className="text-gray-700 font-medium">support@example.com</p>
+              <p className="text-gray-700 ">Telegram chat</p>
+              <p className="text-gray-700 ">(90) 94 441 1407</p>
+              <p className="text-gray-700 ">info@texnikum.uz</p>
             </div>
           </div>
         </div>
